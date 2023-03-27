47c17ab293f1f68b9e252e8c05f8931b1b73f943
Rein_deer <79858134+Ashu313@users.noreply.github.com>
2023-03-27 22:21:28 +0530

    Update front.js

--- src/frontpage/front.js
+++ src/frontpage/front.js
@@ -10,7 +10,7 @@ const Front=({ HandleClientLoad})=>{
     <div className="container">
         <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1Pd20Bab5GXGeoAiK_XGpoxWWnfLCIkcVGA&usqp=CAU" alt=""></img>
        
-   <button type='button' onClick={HandleClientLoad} >Sign in</button>
+   <button type='button' onClick={HandleClientLoad} >Sign-in</button>
     </div>
     );
 }
