4dfb1b7ff0ea21587594a2f58eb636669e43fa46
Rein_deer <79858134+Ashu313@users.noreply.github.com>
2023-04-13 21:28:14 +0530

    Update user.js

--- src/userpage/user.js
+++ src/userpage/user.js
@@ -5,7 +5,7 @@ import React from "react";
 const User=()=>{
     return(
         <>
-        <h1>hello ashutosh how are you ,Do you need any help
+        <h1>hello ashutosh how are you
         </h1>
         </>
     )
